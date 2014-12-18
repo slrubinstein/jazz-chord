a9433662a1c0f1fd6ab3eeaff0a506c8f4e78846
Steve Rubinstein <slrubinstein@gmail.com>
2014-12-18 11:11:43 -0500

    small syntax change in droppable controller

--- client/app/ui-elements/droppable/droppable.directive.js
+++ client/app/ui-elements/droppable/droppable.directive.js
@@ -6,20 +6,23 @@ angular.module('jazzChordApp')
 function droppable() {
   return {
     restrict: 'A',
-    link: function (scope, element, attrs, ctrl) {
-      element.droppable({
-        scope: 'chords',
-        drop: function(event, ui) {
-        	var root = ui.draggable.text(),
-        			type = 'M-triad',
-        			beatIndex = $(event.target).parent().index(),
-        			measureIndex = $(event.target).closest('.measure').index(),
-        			ctrl = scope.$parent.$parent.score;
-
-        	ctrl.switchChords(root, type, beatIndex, measureIndex);
-        	scope.$apply();
-        }
-      })
-    }
+    link: link
   };
+}
+
+function link(scope, element, attrs) {
+  element.droppable({
+    scope: 'chords',
+    drop: function(event, ui) {
+    	var root = ui.draggable.text(),
+    			type = 'M-triad',
+    			beatIndex = $(event.target).parent().index(),
+    			measureIndex = $(event.target).closest('.measure').index(),
+    			ctrl = scope.$parent.$parent.score;
+
+    	ctrl.switchChords(root, type, beatIndex, measureIndex);
+    	scope.$apply();
+    }
+  })
+
 }
