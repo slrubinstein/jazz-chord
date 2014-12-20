2b11a2df20405203482e4a777e3546371bd272a8
Steve Rubinstein <slrubinstein@gmail.com>
2014-12-20 18:57:53 -0500

    Initial commit

---
+++ server/api/song/song.model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var SongSchema = new Schema({
+  title: String,
+  beatsPerMeasure: Number,
+  tempo: Number,
+  author: {type: Schema.Types.ObjectId, ref: 'User'},
+  song: []
+});
+
+module.exports = mongoose.model('Song', SongSchema);
