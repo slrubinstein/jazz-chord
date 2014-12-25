6eeb6218cdefee40183c742a0eec059bca7c8d90
Steve Rubinstein <slrubinstein@gmail.com>
2014-12-25 18:03:28 -0500

    add left and right keypress to move between beats in a measure

--- client/app/score/score.controller.js
+++ client/app/score/score.controller.js
@@ -3,7 +3,20 @@
 angular.module('jazzChordApp')
 	.value('current', {beat: null,
 										 measure: null})
-  .controller('ScoreCtrl', ScoreCtrl);
+  .controller('ScoreCtrl', ScoreCtrl)
+
+  .directive('selectBeat', function($document) {
+  	return function(scope, element, attr) {
+			element.on('click', function() {
+  			$('.select-beat').removeClass('select-beat')
+  			element.addClass('select-beat');
+  		})
+  	}
+  })
+
+
+
+  ;
 
 ScoreCtrl.$inject = ['$scope', 'song', 'musicSubstitutions',
 											'musicChords', 'player', 'current'];
@@ -14,17 +27,68 @@ function ScoreCtrl($scope, song, musicSubstitutions, musicChords,
 	var vm = this;
 
 	vm.deleteMeasure = deleteMeasure;
+	vm.keypress = keypress;
 	vm.makeSubs = makeSubs;
 	vm.song = song.song;
 	vm.substitutions = [];
 	vm.switchChords = switchChords;
 
+	var $doc = angular.element(document);
+
+	$doc.on('keydown', keypress);
+	$scope.$on('$destroy',function(){
+	  $doc.off('keydown', keypress);
+	})
 	
 	function deleteMeasure(index) {
 		song.deleteMeasure(index);
 		current.beat = null;
 		current.measure = null;
 		vm.substitutions = [];
+	}
+
+	function keypress(event) {
+		if (!current.beat || !current.measure) {
+			return;
+		}
+
+		if (event.keyCode === 37) {
+			prevBeat();
+		} else if (event.keyCode === 39) {
+			nextBeat();
+		}
+	}
+
+	function nextBeat() {
+		if (current.measure.index() === song.song.length - 1 &&
+				current.beat.index() === current.measure.children().length - 1) {
+			console.log('end of song')
+			return;
+		}
+
+		if (current.beat.index() === current.measure.children().length - 1) {
+			current.measure = $(current.measure.parent().children()[current.measure.index() + 1])
+			current.beat = $(current.measure.find('[select-beat]')[0])
+			current.beat.trigger('click')
+		} else {
+			$('.select-beat').parent().next().children().trigger('click');
+		}
+	}
+
+	function prevBeat() {
+		if (current.measure.index() === 0 &&
+				current.beat.index() === 1) {
+			console.log('beginning of song')
+			return;
+		}
+
+		if (current.beat.index() === 1) {
+			current.measure = $(current.measure.parent().children()[current.measure.index() - 1])
+			current.beat = $(current.measure.find('[select-beat]').last())
+			current.beat.trigger('click')
+		} else {
+			$('.select-beat').parent().prev().children().trigger('click');
+		}
 	}
 
 	function makeSubs(event, root) {
